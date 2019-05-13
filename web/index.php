c90cb540e4e5d85a13f1b15987dcfa22add685de
Frol Kryuchkov <frol.kruchkov@gmail.com>
2019-05-13 16:14:21 +0300

    Demo set added

--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
-<?php
+<?php // web/index.php
 
 declare(strict_type = 1);
 
-echo "Hello world!<br>";
+if (isset($_GET['page']) && $_GET['page'] === 'foo') {
+    echo "Foo page<br>";
+} else {
+    echo "Index page<br>";
+}
 
 if ($_SERVER['REMOTE_ADDR'] === '127.0.0.1') {
     echo "(Admin stuff)";
